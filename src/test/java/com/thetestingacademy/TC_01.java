b19b2a2f4a268619cc2979272535f91be234ce04
Mamitha01 <mamithameenu1998@gmail.com>
2024-03-14 23:10:50 +0530

    first commit

--- src/test/java/com/thetestingacademy/TC_01.java
+++ src/test/java/com/thetestingacademy/TC_01.java
@@ -1,2 +1,7 @@
-package com.thetestingacademy;public class TC_01 {
+package com.thetestingacademy;
+
+public class TC_01 {
+    public static void main(String[] args) {
+        System.out.println("Hi!!");
+    }
 }
--- src/test/java/com/thetestingacademy/TC_02.java
+++ src/test/java/com/thetestingacademy/TC_02.java
@@ -1,2 +1,17 @@
-package com.thetestingacademy;public class TC_02 {
+package com.thetestingacademy;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class TC_02 {
+
+    @Test
+    @Description("Verify the thetestingacademy website")
+    public void vwologin(){
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://thetestingacademy.com");
+        System.out.println(driver.getTitle());
+    }
 }
